d4cda98bc20b17e8ae01a83b5922d898a16cffc4
muse9312 <muse9312@naver.com>
2024-01-31 22:18:11 +0900

    fix : router 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import './styles/page.scss'
 
 function App() {
   return (
 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/update" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
 
-    <Main />
 
   );
 }
